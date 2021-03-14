17e3faf4eb89ab38753a349ab5c0bde645a30067
carvalho57 <gabrielfeocarvalho57@gmail.com>
2021-03-13 22:09:05 -0300

    Atualizando classe Input

--- ImplementacaoDotNet/ordenacao/Input.cs
+++ ImplementacaoDotNet/ordenacao/Input.cs
@@ -6,6 +6,9 @@ namespace ordenacao
         {
             return new int[] { 29, 7, 11,5, 13,2, 3, 17, 19, 79,  53, 61,83,23,  47, 59, 31, 37, 73, 41, 43,67, 89, 97,71 };            
         }
-
+        public static int[] GetNumbers()
+        {
+            return new int[] {-20,0,9,6,5,7,3,1,-6,35,80,-120,65,15};
+        }
     }
 }
--- ImplementacaoDotNet/ordenacao/Program.cs
+++ ImplementacaoDotNet/ordenacao/Program.cs
@@ -7,8 +7,8 @@ namespace ordenacao
     {
         static void Main(string[] args)
         {
-            var array = new int[] {-20,0,9,6,5,7,3,1,-6,35,80,-120,65,15};
-            // var array = new int[] {4,3,2,1};
+            
+            var array = Input.GetPrimes();
             PrintArray(array);
             Sort.QuickSort(array);
             PrintArray(array);
